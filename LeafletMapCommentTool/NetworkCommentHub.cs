fb2b74232f5158cb1961390c7fb99b5bda1fbd21
Nima Boscarino <nboscarino@hatfieldgroup.com>
2016-12-07 19:23:52 -0800

    Playing with SignalR
    Starting new branch to rework text annotation implementation

---
+++ LeafletMapCommentTool/NetworkCommentHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace LeafletMapCommentTool
+{
+    [HubName("networkComment")]
+    public class NetworkCommentHub : Hub
+    {
+        private static String message = "Hey there";
+
+        public void getMessage()
+        {
+            this.Clients.All.onGetMessage(message);
+        }
+    }
+}
---
+++ LeafletMapCommentTool/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LeafletMapCommentTool.Startup))]
+
+namespace LeafletMapCommentTool
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
