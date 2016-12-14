5ec8d4301ee2717911e865b6679c1d7f3ae8d58f
Nima Boscarino <nboscarino@hatfieldgroup.com>
2016-12-13 16:23:26 -0800

    Alert and greeting fetched from MongoDB
    Just flexing with the driver to see if this stuff works. Time to start
    serializing notes when added

--- LeafletMapCommentTool/NetworkCommentHub.cs
+++ LeafletMapCommentTool/NetworkCommentHub.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace LeafletMapCommentTool
 {
@@ -13,17 +15,32 @@ namespace LeafletMapCommentTool
 
         public void getMessage()
         {
-            this.Clients.Caller.onGetMessage("Ping!!");
-            this.Clients.Others.onGetMessage("A client has connected");
+            var client = new MongoClient();
+            var database = client.GetDatabase("MapCommentToolSignalR");
+            var collection = database.GetCollection<BsonDocument>("info");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("type", "greeting");
+            var greeting = collection.Find(filter).First();
+            filter = Builders<BsonDocument>.Filter.Eq("type", "alert");
+            var alert = collection.Find(filter).First();
+
+            this.Clients.Caller.onGetMessage(greeting["message"]);
+            this.Clients.Others.onGetMessage(alert["message"]);
         }
 
         public void newComment()
         {
+            var client = new MongoClient();
+            var database = client.GetDatabase("MapCommentToolSignalR");
+
             this.Clients.Others.onNewComment();
         }
 
         public void saveComment()
         {
+            var client = new MongoClient();
+            var database = client.GetDatabase("MapCommentToolSignalR");
+
             this.Clients.Others.onSaveComment();
         }
 
